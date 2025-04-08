30b054021248b8ad2ac97cd8854d08700157c43a
hovie.zeng <hovie.zeng@unity.cn>
2025-04-08 20:27:33 +0800

    fix icm clear

--- mario_ppo_icm.py
+++ mario_ppo_icm.py
@@ -9,7 +9,7 @@ from torch.utils.data.sampler import BatchSampler, SubsetRandomSampler
 from env import MultipleEnvironments,obsShape
 import ppo_play
 import numpy as np
-from mario_ppo import PolicyNetwork,ppo_update
+from mario_ppo import PolicyNetwork, RolloutStorage
 
 class Encoder(nn.Module):
     def __init__(self,state_dim, latent_dim):
@@ -83,91 +83,6 @@ class ICMNetWork(nn.Module):
         action_loss = F.cross_entropy(action_p, action)
         intrinsic_reward = 0.5 * F.mse_loss(next_x_l_p, next_x_l, reduction="none").mean(dim=1)
         return intrinsic_reward,forward_loss, action_loss
-
-
-class RolloutStorage(object):
-    def __init__(self, num_steps, num_processes, obs_shape, action_dim, state_size):
-        self.observations = torch.zeros(num_steps + 1, num_processes, *obs_shape)
-        self.rewards = torch.zeros(num_steps, num_processes, 1)
-        self.value_preds = torch.zeros(num_steps + 1, num_processes, 1)
-        self.returns = torch.zeros(num_steps + 1, num_processes, 1)
-        self.action_log_probs = torch.zeros(num_steps, num_processes, 1)
-        self.actions = torch.zeros(num_steps, num_processes, 1)
-        self.masks = torch.ones(num_steps + 1, num_processes, 1)
-        # self.bad_masks = torch.ones(num_steps + 1, num_processes, 1)
-
-        self.num_steps = num_steps
-        self.step = 0
-
-    def to(self, device):
-        self.observations = self.observations.to(device)
-        self.rewards = self.rewards.to(device)
-        self.value_preds = self.value_preds.to(device)
-        self.returns = self.returns.to(device)
-        self.action_log_probs = self.action_log_probs.to(device)
-        self.actions = self.actions.to(device)
-        self.masks = self.masks.to(device)
-
-    def insert(
-            self, current_obs, action, action_log_prob, value_pred, reward, mask
-    ):
-        self.observations[self.step + 1].copy_(current_obs)
-        self.actions[self.step].copy_(action)
-        self.action_log_probs[self.step].copy_(action_log_prob)
-        self.value_preds[self.step].copy_(value_pred)
-        self.rewards[self.step].copy_(reward)
-        self.masks[self.step + 1].copy_(mask)
-
-        self.step = (self.step + 1) % self.num_steps
-    def after_update(self):
-        self.observations[0].copy_(self.observations[-1])
-        self.masks[0].copy_(self.masks[-1])
-
-    def compute_returns(self,next_value,use_gae,gamma,gae_lambda):
-        if use_gae:
-            self.value_preds[-1] = next_value
-            gae = 0
-            for step in reversed(range(self.num_steps)):
-                delta = (
-                    self.rewards[step]
-                    + gamma * self.value_preds[step + 1] * self.masks[step + 1]
-                    - self.value_preds[step]
-                )
-                gae = delta + gamma * gae_lambda * self.masks[step + 1] * gae
-                # gae = gae * self.bad_masks[step + 1]
-                self.returns[step] = gae + self.value_preds[step]
-        else:
-            self.returns[-1] = next_value
-            for step in reversed(range(self.num_steps)):
-                self.returns[step] = (
-                (
-                        self.returns[step + 1] * gamma * self.masks[step + 1]
-                    + self.rewards[step]
-                )
-                # * self.bad_masks[step + 1]
-                    +
-                # + (1 - self.bad_masks[step + 1]) *
-                    self.value_preds[step]
-                )
-
-    def feed_forward_generator(self, advantages, num_mini_batch):
-        num_steps, num_processes = self.rewards.size()[0:2]
-        batch_size = num_processes * num_steps
-        mini_batch_size = batch_size // num_mini_batch
-        sampler = BatchSampler(
-            SubsetRandomSampler(range(batch_size)), mini_batch_size, drop_last=False
-        )
-        for indices in sampler:
-            observations_batch = self.observations[:-1].view(
-                -1, *self.observations.size()[2:]
-            )[indices]
-            actions_batch = self.actions.view(-1, self.actions.size(-1))[indices]
-            return_batch = self.returns[:-1].view(-1, 1)[indices]
-            masks_batch = self.masks[:-1].view(-1, 1)[indices]
-            old_action_log_probs_batch = self.action_log_probs.view(-1, 1)[indices]
-            adv_targ = advantages.view(-1, 1)[indices]
-
-            yield observations_batch, actions_batch, return_batch, masks_batch, old_action_log_probs_batch, adv_targ
 
 
 def icm_ppo_update(icm_net, icm_optimizer, policy_net, optimizer, rollouts, clip_epsilon=0.2,max_grad_norm=1.0):
@@ -250,6 +165,8 @@ def main():
     envs = MultipleEnvironments(nums_processes,world)
     max_actions = 500
 
+    icm_beta = 100
+
     if use_save:
         policy_net = torch.load(save_actor_path, map_location = device, weights_only=False)
         icm_net = torch.load(save_icm_path, map_location = device, weights_only=False)
@@ -267,8 +184,11 @@ def main():
     if use_save:
         policy_net.load_state_dict(torch.load(save_actor_path, map_location = device, weights_only=False).state_dict())
         policy_net.to(device)
+        icm_net.load_state_dict(torch.load(save_icm_path, map_location = device, weights_only=False).state_dict())
+        icm_net.to(device)
     else:
         policy_net._initialize_weights()
+        icm_net._initialize_weights()
 
     optimizer = optim.AdamW(policy_net.parameters(), lr=1e-4, amsgrad=True,weight_decay=0.001)
     icm_optimizer = optim.AdamW(icm_net.parameters(), lr=1e-4, amsgrad=True,weight_decay=0.001)
@@ -308,7 +228,7 @@ def main():
             state = torch.from_numpy(np.concatenate(state, 0))
             next_state_tensor = torch.FloatTensor(state).to(device)
             ireward ,_,_ = icm_net(state_tensor, next_state_tensor, action_probs)
-            reward = reward + ireward.detach().cpu().numpy()
+            reward = reward + ireward.detach().cpu().numpy() * icm_beta
 
             masks = 1 - torch.FloatTensor(done).to(device)
             reward = torch.FloatTensor(reward).to(device)
