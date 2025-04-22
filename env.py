fix icm softmax bug

env.py
@@ -151,6 +151,63 @@ class CustumSingleEnv(gym.Wrapper):
             # 返回当前帧的图像数据
             return self.state
 
+class CustumSingleOptionEnv(gym.Wrapper):
+    def __init__(self, env):
+        super(CustumSingleOptionEnv, self).__init__(env)
+        self.env = env
+        self.observation_space = spaces.Box(low=0, high=255, shape=(84, 84, 4), dtype=np.uint8)
+        self.state = None
+        self.last_render_time = time.perf_counter()
+        self.fps = 240  # 设定目标帧率
+        self.last_score = 0
+        self.hold_jump = 0
+        self.option = 0
+
+    def reset(self):
+        obs = self.env.reset()
+        self.state = obs
+        x_t = cv2.cvtColor(cv2.resize(obs, (84, 84)), cv2.COLOR_BGR2GRAY)[None, :, :]
+        self.last_score = 0
+        return x_t
+
+    def step(self, action):
+        obs, reward, done, info = self.env.step(action)
+        self.state = cv2.cvtColor(obs, cv2.COLOR_RGB2BGR)
+        x_t = cv2.cvtColor(cv2.resize(obs, (84, 84)), cv2.COLOR_BGR2GRAY)[None, :, :]
+
+        reward += (info['score'] - self.last_score) / 40
+        if done:
+            if info["flag_get"]:
+                reward += 50
+            else:
+                reward -= 50
+        # if info['status'] == 'tall':
+        #     reward += 1
+
+        self.last_score = info['score']
+
+        return x_t, reward / 10, done, info
+
+
+    def set_option(self, option):
+        self.env.set_option(option)
+
+    def render(self, mode='human'):
+        current_time = time.perf_counter()
+        elapsed = current_time - self.last_render_time
+        wait_time = max(0, 1 / self.fps - elapsed)
+        time.sleep(wait_time)
+        self.last_render_time = time.perf_counter()
+
+        if mode == 'human':
+            # 如果是人类模式，则显示图片（例如使用 OpenCV 显示）
+            cv2.imshow("Super Mario", self.state)
+            cv2.waitKey(1)
+        elif mode == 'rgb_array':
+            # 返回当前帧的图像数据
+            return self.state
+
+
 class SkipEnv(gym.Wrapper):
     def __init__(self, env):
         super(SkipEnv, self).__init__(env)
@@ -184,14 +241,20 @@ class SkipEnv(gym.Wrapper):
 
         return self.states[None, :, :, :].astype(np.float32), total_reward, done, info
 
+    def set_option(self, option):
+        self.env.set_option(option)
+
 class MultipleEnvironments:
-    def __init__(self, num_envs, world, num_states = 4):
+    def __init__(self, num_envs, world, num_states = 4, use_option = False):
         self.agent_conns, self.env_conns = zip(*[mp.Pipe() for _ in range(num_envs)])
         self.num_states = num_states
         self.num_actions = len(COMPLEX_MOVEMENT)
         self.processes = []
         for index in range(num_envs):
-            process = mp.Process(target=self.run, args=(index, world))
+            if use_option:
+                process = mp.Process(target=self.run_option, args=(index, world))
+            else:
+                process = mp.Process(target=self.run, args=(index, world))
             # self.processes.append(process)
             process.start()
 
@@ -204,6 +267,21 @@ class MultipleEnvironments:
             request, action = self.env_conns[index].recv()
             if request == "step":
                 self.env_conns[index].send(env.step(action.item()))
+            elif request == "reset":
+                self.env_conns[index].send(env.reset())
+            else:
+                raise NotImplementedError
+
+    def run_option(self, index, world):
+        env = gym_super_mario_bros.make(world)
+        env = JoypadSpace(env, COMPLEX_MOVEMENT)
+        env = CustumSingleOptionEnv(env)
+        env = SkipEnv(env)
+        while True:
+            request, action, option = self.env_conns[index].recv()
+            if request == "step":
+                env.set_option(option)
+                self.env_conns[index].send(env.step(action.item(), option.item()))
             elif request == "reset":
                 self.env_conns[index].send(env.reset())
             else:

mario_ppo_icm.py
@@ -29,7 +29,7 @@ class Encoder(nn.Module):
         x = x.reshape(-1,1600)
         x = torch.relu(self.fc1(x))
         ac = self.fc2(x)
-        return torch.softmax(ac,dim=-1)
+        return ac
 
 class Decoder(nn.Module):
     def __init__(self,action_dim, latent_dim):
@@ -155,11 +155,11 @@ def main():
     else:
         torch.manual_seed(996)
     replay_buffer_size = 256
-    use_save = True
+    use_save = False
     save_actor_path = "actor-icm.pth"
     save_icm_path = "icm.pth"
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
-    world = 'SuperMarioBros-3-3-v0'
+    world = 'SuperMarioBros-8-1-v0'
 
     # 创建马里奥环境
     action_dim = len(COMPLEX_MOVEMENT)

mario_ppo_icm_hrl.py
@@ -9,7 +9,7 @@ from env import MultipleEnvironments,obsShape
 import ppo_play
 import numpy as np
 from mario_ppo import RolloutStorage
-from mario_ppo_icm import ICMNetWork
+from mario_ppo_icm import Decoder, Step
 
 class Encoder(nn.Module):
     def __init__(self,state_dim, latent_dim):
@@ -18,48 +18,25 @@ class Encoder(nn.Module):
         self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1, padding_mode='replicate')
         self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, padding_mode='replicate')
         self.pool1 = nn.MaxPool2d(2, 2, padding=1)
-        self.fc1 = nn.Linear(1600, 512)
-        self.fc2 = nn.Linear(512, latent_dim)
+        self.fc1 = nn.Linear(1600, latent_dim)
 
     def forward(self, x):
         x = self.pool1(torch.relu(self.conv1(x)))
         x = torch.relu(self.conv2(x))
         x = torch.relu(self.conv3(x))
         x = x.reshape(-1,1600)
-        x = torch.relu(self.fc1(x))
-        ac = self.fc2(x)
-        return torch.softmax(ac,dim=-1)
+        x = self.fc1(x)
+        return x
 
-
-class PolicyNetwork(nn.Module):
-    def __init__(self,state_dim, action_dim):
-        super().__init__()
-        self.fc1 = nn.Linear(state_dim, 512)
-        self.fc2 = nn.Linear(512, action_dim)
-        self.critic = nn.Linear(512, 1)
-
-    def forward(self, x):
-        x = torch.relu(self.fc1(x))
-        ac = self.fc2(x)
-        val = self.critic(x)
-        return torch.softmax(ac,dim=-1),val
-
-
-    def get_value(self,x):
-        x = torch.relu(self.fc1(x))
-        val = self.critic(x)
-        return val
-
-
-class HRLNetWork(nn.Module):
-    def __init__(self,state_dim, action_dim, latent_dim, goal_dim):
+class ICMNetWork(nn.Module):
+    def __init__(self,state_dim, action_dim, latent_dim):
         super().__init__()
         self.state_dim = state_dim
         self.action_dim = action_dim
         self.latent_dim = latent_dim
-
-        self.encoder = Encoder(state_dim, latent_dim)
-        self.policy = PolicyNetwork(action_dim,latent_dim)
+        self.encoder = Encoder(state_dim,latent_dim)
+        self.decoder = Decoder(action_dim,latent_dim)
+        self.step = Step(action_dim, latent_dim)
         self._initialize_weights()
 
     def _initialize_weights(self):
@@ -69,21 +46,94 @@ class HRLNetWork(nn.Module):
                 # nn.init.xavier_uniform_(module.weight)
                 # nn.init.kaiming_uniform_(module.weight)
                 nn.init.constant_(module.bias, 0)
+    def forward(self, x, next_x, action):
+        x_l = self.encoder(x)
+        next_x_l = self.encoder(next_x)
+        next_x_l_p = self.step(torch.cat([x_l, action], dim=1))
+        action_p = self.decoder(x_l, next_x_l)
+        forward_loss = F.mse_loss(next_x_l_p, next_x_l)
+        action_loss = F.cross_entropy(action_p, action)
+        intrinsic_reward = 0.5 * F.mse_loss(next_x_l_p, next_x_l, reduction="none").mean(dim=1)
+        return intrinsic_reward,forward_loss, action_loss
+
+
+
+class IntraPolicy(nn.Module):
+    def __init__(self,state_dim, action_dim):
+        super().__init__()
+        self.fc1 = nn.Linear(state_dim, 512)
+        self.fc2 = nn.Linear(512, action_dim)
+
     def forward(self, x):
-        x = self.pool1(torch.relu(self.conv1(x)))
-        x = torch.relu(self.conv2(x))
-        x = torch.relu(self.conv3(x))
-        x = x.reshape(-1,1600)
-        return self.policy(x)
+        x = torch.relu(self.fc1(x))
+        ac = self.fc2(x)
+        return torch.softmax(ac,dim=-1)
+
+class OptionPolicy(nn.Module):
+    def __init__(self,state_dim, option_nums):
+        super().__init__()
+        self.fc1 = nn.Linear(state_dim, 512)
+        self.fc2 = nn.Linear(512, option_nums)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        ac = self.fc2(x)
+        return torch.softmax(ac,dim=-1)
+
+class Critic(nn.Module):
+    def __init__(self,state_dim):
+        super().__init__()
+        self.fc1 = nn.Linear(state_dim, 512)
+        self.fc2 = nn.Linear(512, 1)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        ac = self.fc2(x)
+        return ac
+
+class OptionTermination(nn.Module):
+    def __init__(self,state_dim):
+        super().__init__()
+        self.fc1 = nn.Linear(state_dim, 512)
+        self.fc2 = nn.Linear(512, 1)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        ac = self.fc2(x)
+        return torch.sigmoid(ac,dim=-1)
+
+class HRLNetWork(nn.Module):
+    def __init__(self,state_dim, action_dim, latent_dim, option_nums):
+        super().__init__()
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.latent_dim = latent_dim
+
+        self.encoder = Encoder(state_dim, latent_dim)
+        self.option_chooser = OptionPolicy(state_dim, latent_dim)
+        self.option_terminator = OptionTermination(state_dim)
+        self.policy = [IntraPolicy(latent_dim,action_dim) for _ in range(option_nums)]
+        self.critic = Critic(state_dim)
+        self._initialize_weights()
+
+    def _initialize_weights(self):
+        for module in self.modules():
+            if isinstance(module, nn.Conv2d) or isinstance(module, nn.Linear):
+                nn.init.orthogonal_(module.weight, nn.init.calculate_gain('relu'))
+                # nn.init.xavier_uniform_(module.weight)
+                # nn.init.kaiming_uniform_(module.weight)
+                nn.init.constant_(module.bias, 0)
+    def forward(self, x, option):
+        x = self.encoder(x)
+        return self.policy[option](x)
 
     def get_value(self,x):
-        x = self.pool1(torch.relu(self.conv1(x)))
-        x = torch.relu(self.conv2(x))
-        x = torch.relu(self.conv3(x))
-        x = x.reshape(-1,1600)
-        return self.policy.get_value(x)
+        x = self.encoder(x)
+        return self.critic(x)
 
-
+    def choose_option(self,x):
+        x = self.encoder(x)
+        return self.option_chooser(x)
 
 def hrl_icm_ppo_update(icm_net, icm_optimizer, policy_net, optimizer, rollouts, clip_epsilon=0.2,max_grad_norm=1.0):
     wa = 1